e4928506819334f1f4e295aff46b783287a0fa43
paya17 <yjshin1003@naver.com>
2023-12-16 22:30:31 +0900

    N과 M (1)_15649

---
+++ BOJ/Backtracking/N과 M (1)_15649.js
@@ -0,0 +1,37 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+let arr = [];
+for (let i = 1; i <= n; i++) {
+    arr.push(i);
+}
+let visited = new Array(n).fill(false);
+let selected = [];
+
+let answer = '';
+function dfs(arr, depth) {
+    if (depth == m) {
+        for (let i of selected) {
+            answer += arr[i] + ' ';
+        }
+        answer += '\n';
+        return;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (visited[i]) {
+            continue;
+        }
+
+        selected.push(i);
+        visited[i] = true;
+        dfs(arr, depth + 1);
+        selected.pop();
+        visited[i] = false;
+    }
+}
+
+dfs(arr, 0);
+
+console.log(answer);
