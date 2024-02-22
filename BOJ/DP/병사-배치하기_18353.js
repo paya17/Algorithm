76faf731fd239e6c7070e983c525cf00ed0caf60
paya17 <yjshin1003@naver.com>
2024-02-22 22:54:46 +0900

    병사-배치하기_18353

---
+++ BOJ/DP/병사-배치하기_18353.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let input = fs.readFileSync('예제.txt').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
+
+arr.reverse();
+
+let dp = new Array(n).fill(1);
+
+for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[j] < arr[i]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+}
+
+console.log(n - Math.max(...dp));
